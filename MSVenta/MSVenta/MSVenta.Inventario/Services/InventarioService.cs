77504362a59893bb53922c282582df515948f559
06-lis <munozherbas@gmail.com>
2026-07-08 08:23:41 -0400

    fix: resolve CS0168 warning in InventarioService

--- MSVenta/MSVenta/MSVenta.Inventario/Services/InventarioService.cs
+++ MSVenta/MSVenta/MSVenta.Inventario/Services/InventarioService.cs
@@ -66,7 +66,7 @@ namespace MSVenta.Inventario.Services
                 await transaction.CommitAsync();
                 return true;
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 await transaction.RollbackAsync();
                 throw;
